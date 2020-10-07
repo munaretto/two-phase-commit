ae1f033abc95a11e577cd9189b9647b4b21e47a8
Guilherme Munaretto <guilherme.munaretto@acad.pucrs.br>
2020-10-06 23:46:03 -0300

    eh feio mas ta pago

--- src/AtomicMulticast.java
+++ src/AtomicMulticast.java
@@ -2,68 +2,114 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class AtomicMulticast {
+
+	private static ArrayList<String> votes = new ArrayList();
 	public static void main(String[] args) throws IOException {
-    
-    if (args.length != 4) {
+
+		if (args.length != 4) {
 			System.out.println("Uso: AtomicMulticast <iniciador> <grupo> <porta> <nickname>");
 			return;
 		}
 
-		int porta = Integer.parseInt(args[1]);		
-    
-    MulticastSocket socket = new MulticastSocket(porta);
-		InetAddress grupo = InetAddress.getByName(args[0]);
+		boolean iniciador = Boolean.parseBoolean(args[0]);
+		String grupoAddress = args[1];
+		int porta = Integer.parseInt(args[2]);
+		String nick = args[3];
+
+		MulticastSocket socket = new MulticastSocket(porta);
+		InetAddress grupo = InetAddress.getByName(grupoAddress);
+
 		socket.joinGroup(grupo);
-		String nick = args[2];
-		
-		Scanner scanner = new Scanner(System.in);
-		long last_time = 0;
-		
+
+		if (iniciador) {
+			byte[] message = (nick + " " + "VOTE_REQUEST").getBytes();
+			DatagramPacket pacoteInicial = new DatagramPacket(message, message.length, grupo, porta);
+			socket.send(pacoteInicial);
+			System.out.println("Iniciador diz: VOTE_REQUEST");
+		}
+
 		while (true) {
 			try {
-				byte[] entrada = new byte[1024];
-				DatagramPacket pacote = new DatagramPacket(entrada,entrada.length);
-				socket.setSoTimeout(100);
-				socket.receive(pacote);
-				String recebido = new String(pacote.getData(),0,pacote.getLength());
 				
-				String vars[] = recebido.split("\\s");
-				try {
-					if (vars[0].equals(Long.toString(last_time))) {
-						System.out.println("old: " + recebido);
-					} else {
-						System.out.println("new: " + recebido);
-						last_time = Long.parseLong(vars[0]);
+				// Verifica as respostas de todos
+				if (iniciador) {
+					try {
+						int count = 0;
 						
-						byte[] saida = new byte[1024];
-						saida = recebido.getBytes();
-						DatagramPacket pacote2 = new DatagramPacket(saida, saida.length, grupo, porta);
-						socket.send(pacote2);
+						while(count < 5) {
+							byte[] received 				= new byte[1024];
+							DatagramPacket pacote 	= new DatagramPacket(received,received.length);
+							socket.receive(pacote);
+							String recebido 				= new String(pacote.getData(),0,pacote.getLength());
+							String message 					= recebido.split("\\s")[1];
+
+							if (!message.equals("VOTE_REQUEST")) {
+								votes.add(message);
+							}
+							Thread.sleep(1000);
+							count++;
+						}
 						
-						if ("fim".equals(vars[2]))
-							break;
+						String response = votes.stream().allMatch(a -> a.equals("VOTE_COMMIT"))
+							? "GLOBAL_COMMIT"
+							: "GLOBAL_ABORT";
+
+						byte[] responseBytes = (nick + " " + response).getBytes();
+						DatagramPacket pacoteResponse = new DatagramPacket(responseBytes, responseBytes.length, grupo, porta);
+						socket.send(pacoteResponse);
+						System.out.println("Iniciador diz: "+response);
+						break;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
 					}
-					
-				} catch(ArrayIndexOutOfBoundsException e) {
 				}
-								
-			} catch (IOException e){
-			}
-			
-			if (System.in.available() > 0) {
-				String mens = scanner.nextLine();
-				long time = System.currentTimeMillis();
-				byte[] saida = new byte[1024];
-				saida = (Long.toString(time) + " " + nick + " " + mens).getBytes();
-				DatagramPacket pacote = new DatagramPacket(saida, saida.length, grupo, porta);
-				socket.send(pacote);
-			}
+				else {
+					byte[] entrada 				= new byte[1024];
+					DatagramPacket pacote = new DatagramPacket(entrada,entrada.length);
+					socket.setSoTimeout(100);
+					socket.receive(pacote);
+					String recebido 			= new String(pacote.getData(),0,pacote.getLength());
+	
+					String sender 	= recebido.split("\\s")[0];
+					String message 	= recebido.split("\\s")[1];
+	
+					if (message.equals("VOTE_REQUEST")) {
+						Random random = new Random();
+						String vote 	= random.nextBoolean()
+							? "VOTE_COMMIT"
+							:	"VOTE_ABORT";
+						byte[] voteBytes = (nick + " " + vote).getBytes();
+						DatagramPacket votePacote = new DatagramPacket(voteBytes, voteBytes.length, grupo, porta);
+						socket.send(votePacote);
+						System.out.println(sender+" diz: "+message);
+					}
+
+					if (message.equals("GLOBAL_COMMIT") || message.equals("GLOBAL_ABORT")) {
+						System.out.println("Resposta final: ");
+						break;
+					}
+				}
+			} 
+			catch (IOException e){}
 		}
-		
+
 		socket.leaveGroup(grupo);
 		socket.close();
 	}
+
+	public static void setTimeout(Runnable runnable, int delay){
+    new Thread(() -> {
+        try {
+            Thread.sleep(delay);
+            runnable.run();
+        }
+        catch (Exception e){
+            System.err.println(e);
+        }
+    }).start();
+}
 }
